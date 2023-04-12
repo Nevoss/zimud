c33fac663b0cf4f859985e5a4c7f29c655f9490a
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2023-04-12 17:26:38 +0300

    add description for zod config scheme

--- src/schemes/config.ts
+++ src/schemes/config.ts
@@ -1,6 +1,6 @@
 import z from 'zod';
 
 export default z.object({
-	enabled: z.boolean(),
-	packages: z.array(z.string()),
-});
+	enabled: z.boolean().describe('Whether to link packages or not.'),
+	packages: z.array(z.string()).describe('An array of glob patterns to match packages to link.'),
+}).describe('Config schema for plink.');
